2944ea6507b243bd0341f2d690b4545f19bbf764
Sergio Blázquez Segovia <setibs@gmail.com>
2023-04-29 19:02:16 +0200

    V1.0.0

---
+++ Juego_visual.py
@@ -0,0 +1,34 @@
+import keyboard
+import time
+dot = "."
+print(dot)
+while True:
+    print(dot)
+    for x in dot:
+        if keyboard.is_pressed("space"):
+            while keyboard.is_pressed("space"):
+                if len(dot) < 119:
+                    dot += "."
+                    print(dot)
+                else:
+                    print(dot)
+                time.sleep(0.015)
+        elif keyboard.is_pressed("s"):
+            if len(dot) == 1:
+                print("\nHave a good day!")
+                exit()
+            else:
+                for y in dot:
+                    dot = dot[:-1]
+                    print(dot)
+                    time.sleep(0.015)
+                print("Have a good day!")
+                exit()
+        elif len(dot) == 1:
+            time.sleep(0.015)
+            continue
+        else:
+            dot = dot[:-1]
+            print(dot)
+            time.sleep(0.015)
+
