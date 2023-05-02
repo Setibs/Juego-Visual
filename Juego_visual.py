1215396f425aa4a0e61337e3b087eba2dba984ae
Sergio Blázquez Segovia <setibs@gmail.com>
2023-05-02 22:02:51 +0200

    V1.0.02

--- Juego_visual.py
+++ Juego_visual.py
@@ -1,34 +1,57 @@
 import keyboard
 import time
-dot = "."
-print(dot)
-while True:
-    print(dot)
-    for x in dot:
-        if keyboard.is_pressed("space"):
-            while keyboard.is_pressed("space"):
-                if len(dot) < 119: #Límite para una terminal minimizada, ajustar a gusto del consumidor
-                    dot += "."
-                    print(dot)
-                else:
-                    print(dot)
-                time.sleep(0.015)
-        elif keyboard.is_pressed("s"):
-            if len(dot) == 1:
-                print("\nHave a good day!")
-                exit()
-            else:
-                for y in dot:
-                    dot = dot[:-1]
-                    print(dot)
-                    time.sleep(0.015)
-                print("Have a good day!")
-                exit()
-        elif len(dot) == 1:
-            time.sleep(0.015)
-            continue
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.base import runTouchApp
+from kivy.factory import Factory
+
+
+
+class MyGridLayout(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.con = "\n."
+        self.add = ""
+        self.ids.dot_button.opacity = 0
+        Clock.schedule_interval(self.constante, 0.015)
+        Clock.schedule_interval(self.press,0.015)
+    def constante(self,*args):
+        self.ids.dot_label.text += self.con + self.add
+
+    def press(self,*args):
+        if self.ids.dot_button.state == "down":
+            if len(self.add) < 82:
+               self.add += "."
         else:
-            dot = dot[:-1]
-            print(dot)
-            time.sleep(0.015)
+            self.add = self.add[:-1]
+
+
+
+
+
+
+
+
+class MyApp(App):
+
+
+
+
+    def build(self):
+        return MyGridLayout()
+
+
+if __name__=="__main__":
+    MyApp().run()
+
 
