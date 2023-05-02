7406d00c4c0633eae37c6ffd2fce4f9baa755220
Sergio Blázquez Segovia <setibs@gmail.com>
2023-05-03 00:57:12 +0200

    V1.0.1.0

---
+++ main.py
@@ -0,0 +1,46 @@
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
+
+
+
+
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.con = "\n."
+        self.add = ""
+
+        Clock.schedule_interval(self.constante, 0.015)
+        Clock.schedule_interval(self.press,0.015)
+    def constante(self,*args):
+        self.ids.dot_button.text += self.con + self.add
+
+    def press(self,*args):
+        if self.ids.dot_button.state == "down":
+            if len(self.add) < 82:
+               self.add += "."
+        else:
+            self.add = self.add[:-1]
+
+
+
+
+
+
+
+
+class MyApp(App):
+
+
+
+
+    def build(self):
+        return MyGridLayout()
+
+
+if __name__=="__main__":
+    MyApp().run()
+
+
