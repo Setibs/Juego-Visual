7f096314509174e40fb905e6876625506f59c5d9
Sergio Blázquez Segovia <setibs@gmail.com>
2023-05-03 01:26:48 +0200

    V1.0.1.0

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ class MyGridLayout(GridLayout):
         self.con = "\n."
         self.add = ""
 
-        Clock.schedule_interval(self.constante, 0.015)
-        Clock.schedule_interval(self.press,0.015)
+        Clock.schedule_interval(self.constante, 0.005)
+        Clock.schedule_interval(self.press,0.005)
     def constante(self,*args):
         self.ids.dot_button.text += self.con + self.add
 
     def press(self,*args):
         if self.ids.dot_button.state == "down":
-            if len(self.add) < 82:    #Longitud de los puntos, ajustar en función de pantalla
+            if len(self.add) < 88:    #Longitud de los puntos, ajustar en función de pantalla
                self.add += "."
         else:
             self.add = self.add[:-1]
